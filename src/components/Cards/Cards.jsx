687a7dbbf602581f897931ff7e16088e1e7d3796
lucasvtiradentes <lucasvtiradentes@gmail.com>
2020-07-28 16:55:45 -0300

    add first styled card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Card, CardContent, Typography, Grid } from '@material-ui/core'
+import styles from './Cards.module.css'
 
 const Cards = (props) => {
    console.log(props)
    return (
-      <h1>Cards é MEU PAU X</h1>
+      <div className={styles.container}>
+         <Grid container spacing={3} justify="Center">
+         <Grid item component={Card}>
+               <CardContent>
+               <Typography color="textSecondary" gutterBottom>Infected</Typography>
+               <Typography variant="h5">REAL DATA</Typography>
+               <Typography color="textSecondary">REAL DATE</Typography>
+               <Typography variant="body2">Number of active cases of covid19</Typography>
+               </CardContent>
+            </Grid>
+         </Grid>
+      </div>
    )
 }
 
