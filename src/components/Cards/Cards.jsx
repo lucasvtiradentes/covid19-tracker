9f782d46e3ae1067f850ca27f3c22c42daabfa48
lucasvtiradentes <lucasvtiradentes@gmail.com>
2020-07-28 17:16:15 -0300

    add counting animation to first card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import CountUp from 'react-countup'
 import {Card, CardContent, Typography, Grid } from '@material-ui/core'
+
 import styles from './Cards.module.css'
+
 
 const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
 
@@ -16,7 +19,16 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
             <Grid item component={Card}>
                <CardContent>
                <Typography color="textSecondary" gutterBottom>Infected</Typography>
-               <Typography variant="h5">{confirmed.value}</Typography>
+               <Typography variant="h5">
+
+                  <CountUp 
+                     start={0}
+                     end={confirmed.value}
+                     duration={1.5}
+                     separator="."
+                  />
+
+               </Typography>
                <Typography color="textSecondary">REAL DATE</Typography>
                <Typography variant="body2">Number of active cases of covid-19</Typography>
                </CardContent>
