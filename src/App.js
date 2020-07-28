9ff5eed7cfc89714141b383798f0d1f87a352ab7
lucasvtiradentes <lucasvtiradentes@gmail.com>
2020-07-28 16:45:15 -0300

    send fetched data to card component

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import {fetchData} from './api'
 class App extends React.Component {
 
    state = {
-
+      data: {}
    }
    
    async componentDidMount(){
-      const data = await fetchData();
-      console.log(data)
+      const fetchedData = await fetchData();
+      console.log(fetchedData)
+      this.setState({data: fetchedData})
    }
 
    render(){
+      
+      const {data} = this.state;
+
       return (
          <div className={styles.container}>
-            <Cards />
+            <Cards data={data}/>
             <Chart />
             <CountryPicker />
          </div>
       )
+      
    }
 }
 
